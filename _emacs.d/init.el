9a74e2cecfce218bd33a7f90ba4371a1b46e8505
Mattias Jämting <mattias@jamting.se>
2017-04-02 00:22:04 +0200

    Custom faces stuff

--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -484,3 +484,9 @@
      (340 . "#eab700")
      (360 . "#718c00"))))
  '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
